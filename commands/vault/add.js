5499e49ff2c61ebd88865c687ab688aa4f4ee2f7
yourlocalonion <110649288+yourlocalonion@users.noreply.github.com>
2024-03-26 12:22:46 +0800

    pvdbridge

--- commands/vault/add.js
+++ commands/vault/add.js
@@ -1,5 +1,19 @@
 const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, PermissionsBitField } = require('discord.js');
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const { JWT } = require('google-auth-library');
+const fs = require('fs')
 
+const config = JSON.parse(fs.readFileSync("././config.json", 'utf8'));
+
+const serviceAccountAuth = new JWT({
+    email: config.client_email,
+    key: config.private_key.split(String.raw`\n`).join('\n'),
+    scopes: [
+      'https://www.googleapis.com/auth/spreadsheets',
+    ],
+});
+
+const doc = new GoogleSpreadsheet('1Icx_tYyYgdxD_s8A06sKfd410oJ3JI7eXnAgL1e6ncE', serviceAccountAuth);
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -23,6 +37,12 @@ module.exports = {
                 .setRequired(false)),
 	async execute(interaction) {
         await interaction.deferReply({ ephemeral: true });
+        await interaction.editReply('Connecting to PVD')
+        await doc.loadInfo();
+        console.log(`loaded ${doc.title}`);
+        const sheet = doc.sheetsByIndex[0];
+        await interaction.editReply('Creating Embed')
+
         const vaulted = interaction.options.getString('vaulted');
         const tags = interaction.options.getString('tags') ?? 'No Tags';
         const image = interaction.options.getAttachment("image")
@@ -48,14 +68,15 @@ module.exports = {
             .setDescription(tags)
             .setImage(image.url)
             .setFooter({ text: comment })
-		
+
         if (interaction.member.roles.cache.has('1197785502816936016')) {
-            const response = await interaction.editReply({ content: 'This is what the embed will look like:', components: [row], embeds: [embed], ephemeral: true });
+            const response = await interaction.editReply({ content: 'This is what the embed will look like:', components: [row], embeds: [embed]});
             const collectorFilter = i => i.user.id === interaction.user.id;
 
             try {
 	            const confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 60_000 });
                 if (confirmation.customId === 'confirm') {
+                    await sheet.addRow({ person: vaulted, tag: tags, comments: comment });
                     await channel.send({ embeds: [embed] })
                     await confirmation.update({ content: `Added to the vault.`, components: [] });
                 } else if (confirmation.customId === 'cancel') {
